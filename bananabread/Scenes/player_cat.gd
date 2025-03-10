ab0c9e74fccec62c95d0bf2111f92dc1410b47b5
nicey <vuminhanh.dang@student.unimelb.edu.au>
2025-03-10 16:10:25 +1100

    attack mechanism, frog flips when hits a wall, changed jump key to uparrow

--- bananabread/Scenes/player_cat.gd
+++ bananabread/Scenes/player_cat.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
+class_name PlayerCat
 
-
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
-
+const RAW_DAMAGE = 1
+const WEAPON_DAMAGE = 2
+var health = 9
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,7 +13,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("jump_with_arrow_key") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -23,3 +25,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func takes_damage(damage):
+	if health > 0:
+		health = health - damage
+	else:
+		print("0 health")
+
+func _on_damageable_body_entered(body: Node2D) -> void:
+	print("-1 health")
+	takes_damage(EnemyFrog.DAMAGE)
