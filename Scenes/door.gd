6c8e19abdcd9b8e6298a2bba122d974167463462
nicey <vuminhanh.dang@student.unimelb.edu.au>
2025-03-11 15:37:10 +1100

    connected 2 levels

--- Scenes/door.gd
+++ Scenes/door.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const FILE_BEGIN = "res://levels/Level"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +13,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
-	if "PlayerCat" in body.name:
-		print("door opened")
+	if body.is_in_group("PlayerCat"):
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() + 1
 		
-	pass # Replace with function body.
+		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		get_tree().change_scene_to_file(next_level_path)
+		print("next level")
